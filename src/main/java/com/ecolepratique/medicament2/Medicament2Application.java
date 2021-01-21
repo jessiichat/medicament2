e32530073b0cc9fbd6b555defcaf2e18d3b9de2a
jessiichat <jessiichat@gmail.com>
2021-01-21 09:22:06 +0100

    Revert "Ajout message "Modification 1""
    This reverts commit 293cca32f81d09b0912c68222cd0436d6ecaa4b0.

--- src/main/java/com/ecolepratique/medicament2/Medicament2Application.java
+++ src/main/java/com/ecolepratique/medicament2/Medicament2Application.java
@@ -49,9 +49,6 @@ public class Medicament2Application {
 		medicamentService.create(medicament4);
 		
 		System.out.println(medicamentService.list());
-		
-		// Modification 1
-		System.out.println("Modification 1");
 	}
 
 }
